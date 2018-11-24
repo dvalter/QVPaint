88a4b2b50888f350008197ec56a12691fed76edc
Dmitry Valter <38795282+dvalter@users.noreply.github.com>
2018-11-25 01:53:11 +0300

    Add licenses

--- qvpline.h
+++ qvpline.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2017-2018 Dmitry Valter
+ *
+ * QVPaint is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, version 3
+ *  or the GNU Lesser General Public License, version 3 as published
+ *  by the Free Software Foundation or either under the terms of the
+ *  Do What the Fuck You Want to Public License, Version 2 with
+ *  The No Warranty Exception 1.0.
+ *
+ *  QVPaint is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License,  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License,
+ *  GNU Lesser General Public License and Do What the Fuck You
+ *  Want to Public License along with QVPaint. If not, see
+ *  <https://www.gnu.org/licenses/> and <http://www.wtfpl.net>.
+ */
+
 #ifndef QVPLINE_H
 #define QVPLINE_H
 
